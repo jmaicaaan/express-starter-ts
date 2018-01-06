f9af5f15806c3c5fad584c28a3df10a63b218741
jmaicaaan <jmsantosss29@yahoo.com>
2018-01-07 01:15:00 +0800

    Fixed runtime and package

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,7 @@ import { bootstrapContainers, bootstrapDB, BootstrapServer } from './utils';
 
 bootstrapContainers();
 
-const app = Container.get(BootstrapServer).bootApp();
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => { console.log(`The server is listening at http://localhost:${port}`); });
+const server = Container.get(BootstrapServer);
+server.startListening();
 
 bootstrapDB();
