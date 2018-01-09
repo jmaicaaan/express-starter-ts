2585800c51b9eb77a89e29127a1a7b2d9ea4ec67
Cedrick Mandocdoc <cmandocdoc@isbx..com>
2018-01-09 16:42:00 +0800

    edit orm config

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,15 @@
-
-require('reflect-metadata');
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || `development`;
 let config;
-switch (env) {
-  case 'development':
-    config = require(__dirname + '/config/ormconfig.json');
-    break;
-  case 'test':
-    config = require(__dirname + '/config/ormconfig.test.json');
-    break;
-  default:
-    config = require(__dirname + '/config/ormconfig.json');
-      break;
+try {
+  const path = process.cwd() + `/config/ormconfig.${process.env.NODE_ENV}.json`;
+  config = require(path);
+} catch (error) {
+  const path = process.cwd() + `/config/ormconfig.json`;
+  config = require(path);
 }
-config.entities = [ __dirname + '/dist/entities/*.entity.js' ];
-config.migrations = [ __dirname + '/dist/migrations/*.js' ]
+config.entities = [ process.cwd() +  `/dist/entities/index.js`],
+config.migrations = [ process.cwd() + `/dist/migrations/*.js` ]
 config.cli = {
-  migrationsDir: '/src/migrations/'
+  migrationsDir: `/src/migrations/`
 }
 module.exports = config;
