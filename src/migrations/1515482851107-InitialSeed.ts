46e164d675e17a591243e627cbc976b7193437af
John Michael Santos <jsantos@isbx.com>
2018-01-11 16:49:34 +0800

    Added some into initial seeds

--- src/migrations/1515482851107-InitialSeed.ts
+++ src/migrations/1515482851107-InitialSeed.ts
@@ -6,15 +6,32 @@ export class InitialSeed1515482851107 implements MigrationInterface {
       // seed role table
       await queryRunner.query(`
         INSERT INTO "role"(name)
-          VALUES ('Admin');
+          VALUES ('Admin'), ('User');
+      `);
+
+      await queryRunner.query(`
+        INSERT INTO "user"(email, password)
+          VALUES ('admin@example.com', 'password123'),
+                ('user@example.com', 'password123');
+      `);
+
+      await queryRunner.query(`
+        SELECT u.id, r.id from
+          ( SELECT id from "user" where email='admin@example.com' ) as u,
+            ( SELECT id from "role" where name='Admin') as r
+      `);
+
+      await queryRunner.query(`
+        SELECT u.id, r.id from
+          ( SELECT id from "user" where email='user@example.com' ) as u,
+            ( SELECT id from "role" where name='User') as r
       `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-      await queryRunner.query(`
-        DELETE FROM "role"
-          where name = 'Admin'
-      `);
+      await queryRunner.query(`DELETE FROM "role_mapping";`);
+      await queryRunner.query(`DELETE FROM "role";`);
+      await queryRunner.query(`DELETE FROM "user";`);
     }
 
 }
