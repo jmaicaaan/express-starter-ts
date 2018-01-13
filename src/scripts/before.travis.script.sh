7fe8bb85e542300bf983948d9b73b278a6917476
jmaicaaan <jmsantosss29@yahoo.com>
2018-01-13 23:43:44 +0800

    try

--- src/scripts/before.travis.script.sh
+++ src/scripts/before.travis.script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 psql -U postgres -c "CREATE DATABASE sample_test;" &&
 psql -U postgres -c "CREATE USER sample WITH PASSWORD 'sample';" &&
 psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE sample_test TO sample;" &&
