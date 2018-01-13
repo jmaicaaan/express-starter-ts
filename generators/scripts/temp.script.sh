fb957ce37c7740adb15710eba24a0bb9cd9fff49
jmaicaaan <jmsantosss29@yahoo.com>
2018-01-13 23:51:25 +0800

    try again

--- generators/scripts/temp.script.sh
+++ generators/scripts/temp.script.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-psql -U postgres -c "CREATE DATABASE <%= databaseName %>"
-psql -U postgres -c "CREATE USER <%= username %> WITH PASSWORD '<%= password %>'"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE <%= databaseName %> TO <%= username %>;"
+#!/usr/bin/env bash
+psql -U postgres -c "CREATE DATABASE <%= databaseName %>;" &&
+psql -U postgres -c "CREATE USER <%= username %> WITH PASSWORD '<%= password %>';" &&
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE <%= databaseName %> TO <%= username %>;" &&
 npm run dbmigrate:up
