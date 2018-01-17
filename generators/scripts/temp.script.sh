72dbf0be2aaa32a13d0d73af23556cb3144de976
John Michael Santos <jsantos@isbx.com>
2018-01-17 08:21:59 +0800

    Test semap

--- generators/scripts/temp.script.sh
+++ generators/scripts/temp.script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-psql -U runner -c "CREATE DATABASE <%= databaseName %>;"
-psql -U runner -c "CREATE USER <%= username %> WITH PASSWORD '<%= password %>'"
-psql -U runner -c "GRANT ALL PRIVILEGES ON DATABASE <%= databaseName %> TO <%= username %>;"
+psql -U DATABASE_POSTGRESQL_USERNAME -c "CREATE DATABASE <%= databaseName %>;"
+psql -U DATABASE_POSTGRESQL_USERNAME -c "CREATE USER <%= username %> WITH PASSWORD '<%= password %>'"
+psql -U DATABASE_POSTGRESQL_USERNAME -c "GRANT ALL PRIVILEGES ON DATABASE <%= databaseName %> TO <%= username %>;"
 npm run dbmigrate:up
