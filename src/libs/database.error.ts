a0c3b9462937db29c659ea952b6582196b952b02
cedrick mandocdoc <cedrickmandocdoc@gmail.com>
2018-02-10 23:12:25 +0800

    remove unnecessary code

--- src/libs/database.error.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from 'make-error';
-
-export class DatabaseError extends BaseError {
-
-  public constructor(message: string) {
-    super();
-    this.message = message;
-  }
-}
