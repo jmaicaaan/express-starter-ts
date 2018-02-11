240c9b67e5814a38731dfb2d19315922dc68ed36
cedrick mandocdoc <cedrickmandocdoc@gmail.com>
2018-02-11 09:00:38 +0800

    add dummy data

--- src/database/seeds/user.seed.ts
+++ src/database/seeds/user.seed.ts
@@ -1,13 +1,32 @@
+import { Chance} from 'chance';
 import { Service } from 'typedi';
 import { Connection, EntityManager, getRepository, QueryRunner, Repository } from 'typeorm';
 import { SeedInterface } from '../../libs/seed.interface';
+import { BcryptService } from '../../services/brcrypt.service';
 import { User } from '../entities/user.entity';
 import { UserRepository } from '../repositories/user.repository';
 
 @Service()
 export class UserSeed implements SeedInterface {
 
+  private bcryptService: BcryptService;
+
+  public constructor(bcryptService: BcryptService) {
+    this.bcryptService = bcryptService;
+  }
+
   public async seed(connection: Connection): Promise<any> {
+    const chance = new Chance();
+    const data: User[] = [];
+    let i = 0;
+    while (i < 100) {
+      data.push({
+        email: chance.email({domain: 'example.com'}),
+        password: await this.bcryptService.hashString('test'),
+        roles: [{ id: 1, name: 'admin' }]
+      });
+      i++;
+    }
     const queryRunner = connection.createQueryRunner();
     const entityManager = connection.createEntityManager();
     const userRepository = connection.getCustomRepository(UserRepository);
@@ -32,5 +51,6 @@ export class UserSeed implements SeedInterface {
         roles: [{ id: 1, name: 'Admin' }]
       }
     ]);
+    await userRepository.save(data);
   }
 }
